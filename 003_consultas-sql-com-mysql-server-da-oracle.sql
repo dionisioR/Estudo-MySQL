4cc2a344926bfcf6b5475389226b647b7a251d1e
dionisioR <72356766+dionisioR@users.noreply.github.com>
2024-11-12 19:25:41 -0300

    Update 2024-11-12   19h25
    Update 2024-11-12   19h25

--- 003_consultas-sql-com-mysql-server-da-oracle.sql
+++ 003_consultas-sql-com-mysql-server-da-oracle.sql
@@ -2,4 +2,11 @@
 -- <condicao> - texto a ser usado.
 -- O símbolo % é usado para representar qualquer registro genérico.
 -- Equivale ao * quando manipulado em arquivos.
+-- LIKE '%soares%' - todo texto que possui soares
+-- LIKE '%soares' - todo texto que termina em soares
+
+select * from tabela_de_produtos where SABOR like '%maça%';
+select * from tabela_de_produtos where SABOR like '%maça%' and embalagem = 'pet';
+
+
 
